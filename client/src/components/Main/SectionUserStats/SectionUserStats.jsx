3e037bd676fe298913d4879df4319515de28f51b
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-04-04 18:55:00 +0300

    install chartist

--- client/src/components/Main/SectionUserStats/SectionUserStats.jsx
+++ client/src/components/Main/SectionUserStats/SectionUserStats.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import { Link, NavLink } from "react-router-dom";
-import $ from "jquery";
-import "datatables.net";
-import "datatables.net-dt/css/jquery.dataTables.min.css";
-import Users from "../../../server/users.json";
+import Chartist from "chartist/dist/chartist";
+import "chartist/dist/chartist.min.css";
 import "./SectionUserStats.scss";
-
-$.fn.DataTable.ext.pager.numbers_length = 5;
 
 export default class SectionStats extends Component {
   static propTypes = {
@@ -38,6 +33,52 @@ export default class SectionStats extends Component {
   };
   componentDidMount() {
     console.log("mount");
+    var data = {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ],
+      series: [[5, 2, 4, 2, 0]]
+    };
+    var options = {
+      width: 300,
+      height: 200
+    };
+    var responsiveOptions = [
+      [
+        "screen and (min-width: 641px) and (max-width: 1024px)",
+        {
+          seriesBarDistance: 10,
+          axisX: {
+            labelInterpolationFnc: function(value) {
+              return value;
+            }
+          }
+        }
+      ],
+      [
+        "screen and (max-width: 640px)",
+        {
+          seriesBarDistance: 5,
+          axisX: {
+            labelInterpolationFnc: function(value) {
+              return value[0];
+            }
+          }
+        }
+      ]
+    ];
+    new Chartist.Line(".ct-chart", data, options, responsiveOptions);
   }
 
   render() {
@@ -65,7 +106,9 @@ export default class SectionStats extends Component {
               {first_name} {last_name}
             </h3>
           </div>
-          <div className="userStats__schedule-wrap">table</div>
+          <div className="userStats__schedule-wrap">
+            <div class="ct-chart ct-perfect-fourth"></div>
+          </div>
         </div>
       </section>
     );
