3acdf2bf6496474be2af35c3b82cdf1c32285ed7
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-04-06 15:33:59 +0300

    end user stats

--- client/src/components/Main/SectionUserStats/SectionUserStats.jsx
+++ client/src/components/Main/SectionUserStats/SectionUserStats.jsx
@@ -1,84 +1,116 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link, NavLink } from "react-router-dom";
-import Chartist from "chartist/dist/chartist";
-import "chartist/dist/chartist.min.css";
+import $ from "jquery";
+import Chart from "chart.js";
 import "./SectionUserStats.scss";
+// {"user_id":119,"date":"2019-10-02","page_views":724,"clicks":505}
+
+const renderChart = async (domElement, data) => {
+  const sort = data.map((element) => element.x).sort();
+  console.log(sort);
+  new Chart(domElement, {
+    type: "line",
+    responsive: false,
+    data: {
+      datasets: [
+        {
+          data: data,
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          lineTension: 0.2,
+          pointRadius: 0,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        responsive: true,
+        xAxes: [
+          {
+            ticks: {
+              display: true,
+            },
+            type: "time",
+            time: {
+              min: sort[0],
+              max: sort[-1],
+              unit: "month",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: true,
+              color: "black",
+            },
+            ticks: {
+              display: true,
+              min: 0,
+              max: 1000,
+              stepSize: 200,
+            },
+          },
+        ],
+      },
+      legend: {
+        display: false,
+      },
+    },
+  });
+};
 
 export default class SectionStats extends Component {
   static propTypes = {
     route: PropTypes.shape({
       history: PropTypes.shape({
-        push: PropTypes.func.isRequired
+        push: PropTypes.func.isRequired,
       }).isRequired,
       location: PropTypes.shape({
-        search: PropTypes.string.isRequired
+        search: PropTypes.string.isRequired,
       }).isRequired,
       match: PropTypes.shape({
         params: PropTypes.shape({
-          id: PropTypes.string
-        })
-      }).isRequired
-    })
+          id: PropTypes.string,
+        }),
+      }).isRequired,
+    }),
   };
   state = {
     user: {
-      id: 4,
       first_name: "Samuel",
       last_name: "Frost",
-      email: "aosullivan6@sphinn.com",
-      gender: "Male",
-      ip_address: "218.183.185.92"
-    }
+      views: [
+        { x: "2018-01-21", y: 100 },
+        { x: "2018-01-23", y: 100 },
+        { x: "2018-02-05", y: 40 },
+        { x: "2018-03-08", y: 300 },
+        { x: "2018-03-16", y: 300 },
+        { x: "2018-03-28", y: 300 },
+        { x: "2018-04-16", y: 10 },
+        { x: "2018-05-17", y: 900 },
+      ],
+      clicks: [
+        { x: "2018-01-21", y: 100 },
+        { x: "2018-01-23", y: 200 },
+        { x: "2018-02-05", y: 550 },
+        { x: "2018-03-08", y: 200 },
+        { x: "2018-03-08", y: 850 },
+        { x: "2018-03-16", y: 123 },
+        { x: "2018-03-28", y: 654 },
+        { x: "2018-04-16", y: 10 },
+      ],
+    },
   };
   componentDidMount() {
-    console.log("mount");
-    var data = {
-      labels: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "Mai",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-      ],
-      series: [[5, 2, 4, 2, 0]]
-    };
-    var options = {
-      width: 300,
-      height: 200
-    };
-    var responsiveOptions = [
-      [
-        "screen and (min-width: 641px) and (max-width: 1024px)",
-        {
-          seriesBarDistance: 10,
-          axisX: {
-            labelInterpolationFnc: function(value) {
-              return value;
-            }
-          }
-        }
-      ],
-      [
-        "screen and (max-width: 640px)",
-        {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function(value) {
-              return value[0];
-            }
-          }
-        }
-      ]
-    ];
-    new Chartist.Line(".ct-chart", data, options, responsiveOptions);
+    // fetch("/user/:id")
+    //   .then((res) => res.json())
+    //   .then((users) => this.setState({ users }));
+    const clicksChart = $("#clicksChart");
+    const viewsChart = $("#viewsChart");
+    renderChart(clicksChart, this.state.user.clicks);
+    renderChart(viewsChart, this.state.user.views);
   }
 
   render() {
@@ -107,7 +139,16 @@ export default class SectionStats extends Component {
             </h3>
           </div>
           <div className="userStats__schedule-wrap">
-            <div class="ct-chart ct-perfect-fourth"></div>
+            <div className="userStats__chart">
+              <h4>Clicks</h4>
+              <canvas id="clicksChart"></canvas>
+            </div>
+            <div className="userStats__chart">
+              <h4>Vievs</h4>
+              <div className="chart-container">
+                <canvas id="viewsChart"></canvas>
+              </div>
+            </div>
           </div>
         </div>
       </section>
