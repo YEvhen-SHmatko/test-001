58055394fed1fc4aeb46e8837d4d6efc5a9e78d4
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2020-04-09 23:34:38 +0300

    destructurisation database.js

--- server/src/modules/db/database.js
+++ server/src/modules/db/database.js
@@ -7,87 +7,54 @@ const {
 } = require("../../../config");
 
 const createTableUsers = (db) => {
-  const allUsers = JSON.parse(
-    fs.readFileSync(usersDbPath, (err, data) => {
-      if (err) throw err;
-      return data;
-    })
-  );
-  // console.log(users);
-  db.run(
-    `CREATE TABLE users (
-          id INTEGER PRIMARY KEY,
-          first_name text, 
-          last_name text, 
-          email text UNIQUE, 
-          gender text,
-          ip_address text,
-          CONSTRAINT email_unique UNIQUE (email)
-          )`,
-    (err) => {
-      if (err) {
-        // Table already created
-      } else {
-        // Table just created, creating some rows
-        console.log("CREATE TABLE users");
-        const insert =
-          "INSERT INTO users (id, first_name, last_name, email, gender, ip_address) VALUES (?,?,?,?,?,?)";
-        allUsers.map((user) => {
-          db.run(insert, [
-            user.id,
-            user.first_name,
-            user.last_name,
-            user.email,
-            user.gender,
-            user.ip_address,
-          ]);
-        });
-      }
+  const allUsers = JSON.parse(fs.readFileSync(usersDbPath));
+  const create = `CREATE TABLE users ( id INTEGER PRIMARY KEY, first_name text, last_name text, email text UNIQUE, gender text, ip_address text, CONSTRAINT email_unique UNIQUE (email))`;
+
+  const insert =
+    "INSERT INTO users (id, first_name, last_name, email, gender, ip_address) VALUES (?,?,?,?,?,?)";
+
+  const cbCreate = (err) => {
+    if (err) {
+      // console.log(err);
+      return;
     }
-  );
+    allUsers.map((user) => {
+      db.run(insert, [
+        user.id,
+        user.first_name,
+        user.last_name,
+        user.email,
+        user.gender,
+        user.ip_address,
+      ]);
+      console.log("users", user.id);
+    });
+  };
+  db.run(create, cbCreate);
 };
 const createTableUserStats = (db) => {
-  const usersStat = JSON.parse(
-    fs.readFileSync(usersStatisticDbPath, (err, data) => {
-      if (err) throw err;
-      return data;
-    })
-  );
-  // console.log(users);
-  db.run(
-    `CREATE TABLE users_statistic (
-      user_id INTEGER,
-      date text, 
-      page_views INTEGER, 
-      clicks INTEGER
-          )`,
-    (err) => {
-      if (err) {
-        // Table already created
-      } else {
-        // Table just created, creating some rows
-        console.log("CREATE TABLE users_statistic");
-        const insert =
-          "INSERT INTO users_statistic (user_id, date, page_views, clicks) VALUES (?,?,?,?)";
-        usersStat.map((user) => {
-          db.run(insert, [
-            user.user_id,
-            user.date,
-            user.page_views,
-            user.clicks,
-          ]);
-        });
-      }
+  const usersStat = JSON.parse(fs.readFileSync(usersStatisticDbPath));
+  const create = `CREATE TABLE users_statistic ( user_id INTEGER, date text, page_views INTEGER, clicks INTEGER)`;
+  const insert =
+    "INSERT INTO users_statistic (user_id, date, page_views, clicks) VALUES (?,?,?,?)";
+  const cbCreate = (err) => {
+    if (err) {
+      // console.log(err);
+      return;
     }
-  );
+    usersStat.map((user) => {
+      db.run(insert, [user.user_id, user.date, user.page_views, user.clicks]);
+      console.log("stats", user.user_id);
+    });
+  };
+  db.run(create, cbCreate);
 };
-let db = new sqlite3.Database(DBSOURCE, async (err) => {
+let db = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) {
-    // Cannot open database
     console.error(err.message);
   } else {
-    await createTableUsers(db);
-    await createTableUserStats(db);
+    createTableUsers(db);
+    createTableUserStats(db);
   }
 });
 
